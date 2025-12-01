506969d26f2416ff96872ab61f10e39fec12a64b
Mingjing Tong <mingjing.tong@noaa.gov>
2025-12-01 10:39:12 -0500

    Update build environment to spack-stack-1.9.2

--- site/environment.intel.sh
+++ site/environment.intel.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #***********************************************************************
 #*                   GNU Lesser General Public License
 #*
@@ -41,32 +41,30 @@ case $hostname in
         module rm gcc
         module load intel-classic/2023.2.0
         module unload cray-libsci
-        module load cray-hdf5
-        module load cray-netcdf
+        module load cray-hdf5/1.12.2.11
+        module load cray-netcdf/4.9.0.9
         module load craype-hugepages4M
         #module load cmake/3.23.1
         #module load libyaml/0.2.5
 
-        module use /ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/Core
+        module use /ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core
         module load stack-intel/2023.2.0
-        module load stack-cray-mpich/8.1.29
+        module load stack-cray-mpich/8.1.30
 
-        module load esmf/8.6.0
-        module load mapl/2.40.3-esmf-8.6.0
+        module load esmf/8.8.0
       else
         echo " HPCSTACK = Y "
         module load   PrgEnv-intel
         module load intel-classic/2023.2.0
-        module load cray-hdf5
-        module load cray-netcdf
+        module load cray-hdf5/1.12.2.11
+        module load cray-netcdf/4.9.0.9
         module load craype-hugepages4M
 
-        module use /ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/Core
+        module use /ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core
         module load stack-intel/2023.2.0
-        module load stack-cray-mpich/8.1.29
+        module load stack-cray-mpich/8.1.30
 
-        module load esmf/8.6.0
-        module load mapl/2.40.3-esmf-8.6.0
+        module load esmf/8.8.0
 
         module load bacio/2.4.1
         module load sp/2.5.0
@@ -85,6 +83,9 @@ case $hostname in
       export LD=ftn
       export TEMPLATE=site/intel.mk
       export LAUNCHER=srun
+
+      #need to add this for dynamically linking on GAEA
+      export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
 
       # highest level of AVX support
       export AVX_LEVEL=-march=core-avx-i
